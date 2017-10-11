76a38f50a38cf3f841420938061ed36b72539669
hamholla <hamhollett@gmail.com>
2017-10-11 16:52:29 -0400

    add glamorous and responsive styling to homepage

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import { styles } from './styles';
+import { Header, Layout, Background, Copy, Logo, CopyBox, ImgBox } from './styles';
 import logo from '../../assets/imgs/badge.png';
 
+
 const Home = () => (
-  <div className="homeBackground" style={styles.background}>
-    <div className="container" style={styles.layout}>
-      <div className="row">
-        <div className="col-md-6">
-          <img src={logo} alt="logo" style={styles.logo} />
+  <Background>
+    <Layout>
+      <ImgBox>
+        <div className="container">
+          <div className="row">
+            <ImgBox className="col-md-6">
+              <Logo src={logo} alt="logo" />
+            </ImgBox>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-md-6 col-md-offset-6">
-          <h3 style={styles.header}>Who we are</h3>
-          <p style={styles.copy}>
-            Farm Envy is a new program that allows you to easily manage all the
-            details of your CSA so that your customers are happy and you have more
-            time for farming. Farm Envy allows your customers to sign up for your
-            CSA online, it will automatically manage payments, organize each pick up
-            day and drop off, and allow you to communicate easily with your members.
-            Your CSA members will love it because your CSA will run smoothly with
-            less mistakes and they can manage most things themselves online!
-            It makes it easy for members to sign up, make changes, pay, and even
-            let you know about special requests. We are farmers so we know
-            how difficult these things are. Farm Envy makes it easy!
-          </p>
-          <p style={styles.copy}>Coming Soon...</p>
+      </ImgBox>
+      <CopyBox>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-6">
+              <Header>Who we are</Header>
+              <Copy>
+                Farm Envy is a new program that allows you to easily manage all the
+                details of your CSA so that your customers are happy and you have more
+                time for farming. Farm Envy allows your customers to sign up for your
+                CSA online, it will automatically manage payments, organize each pick up
+                day and drop off, and allow you to communicate easily with your members.
+                Your CSA members will love it because your CSA will run smoothly with
+                less mistakes and they can manage most things themselves online!
+                It makes it easy for members to sign up, make changes, pay, and even
+                let you know about special requests. We are farmers so we know
+                how difficult these things are. Farm Envy makes it easy!
+              </Copy>
+              <Copy>Coming Soon...</Copy>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
+      </CopyBox>
+    </Layout>
+  </Background>
 );
 
 export default Home;
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,26 +1,63 @@
-export const styles = {
-  background: {
-    background: 'linear-gradient(to right bottom, #ffffff 50%, #32B989 50%)',
-    height: '100vh',
-  },
-  layout: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignContent: 'center',
-    height: '100vh',
-  },
-  logo: {
-    width: '340px',
-  },
-  copy: {
-    color: '#ffffff',
-    textAlign: 'left',
-  },
-  header: {
-    textAlign: 'left',
-    color: '#ffffff',
-    fontSize: '18px',
-    fontWeight: 'bold',
-  },
+import glamorous from 'glamorous';
+
+const mediaQueries = {
+  tablet: '@media only screen and (max-width: 992px)',
 };
+
+export const MyStyledDiv = glamorous.div({
+  fontSize: 20,
+  textAlign: 'center',
+});
+
+export const Background = glamorous.div({
+  background: 'linear-gradient(to right bottom, #ffffff 50%, #32B989 50%)',
+  height: 'auto',
+  [mediaQueries.tablet]: {
+    background: '#32B989',
+  },
+});
+
+export const Layout = glamorous.div({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignContent: 'center',
+  minHeight: '100vh',
+  [mediaQueries.tablet]: {
+    height: 'auto',
+  },
+});
+
+export const Copy = glamorous.p({
+  color: '#ffffff',
+  textAlign: 'left',
+});
+
+export const ImgBox = glamorous.div({
+  [mediaQueries.tablet]: {
+    background: '#ffffff',
+    padding: '40px 0px 40px 0px',
+  },
+});
+
+export const CopyBox = glamorous.div({
+  [mediaQueries.tablet]: {
+    background: '#32B989',
+    padding: '20px',
+  },
+});
+
+export const Header = glamorous.p({
+  textAlign: 'left',
+  color: '#ffffff',
+  fontSize: '22px',
+  fontWeight: 'bold',
+});
+
+export const Logo = glamorous.img({
+  maxWidth: '340px',
+  width: '100%',
+  [mediaQueries.tablet]: {
+    maxWidth: '250px',
+  },
+});
