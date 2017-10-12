4f0e33a9cf804f5b7fe9f0cff4b10cb0a3424a8f
John Mason <john@johnmason.io>
2017-10-12 15:59:05 -0400

    Allow CI to make production releases
    Now, to make a production release just change the STAGING file
    and jenkins will handle the rest!

--- api/app/controllers/health_controller.rb
+++ api/app/controllers/health_controller.rb
@@ -3,7 +3,6 @@ class HealthController < ApplicationController
 
   def index
     render json: {
-      version: Rails.configuration.version,
       server: Rails.configuration.server,
       last_updated: Rails.configuration.last_updated
     }
