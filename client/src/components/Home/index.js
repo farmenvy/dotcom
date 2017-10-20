9f484e437346ea26234fc48dc7bc5e41aad43f4d
John Mason <john@johnmason.io>
2017-10-20 13:27:19 -0400

    mobile friendly

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,7 +4,10 @@ import styled, { ThemeProvider } from 'styled-components';
 import defaultTheme from '../styles/DefaultTheme';
 import logo from '../../assets/imgs/logo.svg';
 
-const Logo = () => (<img src={logo} alt="logo" />);
+const Container = styled.div`
+  padding: 50px 70px;
+`;
+
 const Copy = styled.div`
   margin: 1em 0em;
   font-size: 16px;
@@ -13,53 +16,65 @@ const Copy = styled.div`
   color: #24405d;
 `;
 
+const SignupText = styled.div`
+  margin-top: 3em;
+  opacity: 0.5;
+  font-size: 24px;
+  font-weight: 300;
+  line-height: 1.42;
+  letter-spacing: -0.5px;
+  text-align: center;
+  color: #353535;
+`;
+
 const Home = () => (
   <ThemeProvider theme={defaultTheme} >
-    <Grid fluid style={{ margin: '100px 150px' }}>
-      <Row>
-        <Col md={6} >
-          <div className="pull-left" style={{ marginBottom: '25px' }}>
-            <Logo />
-          </div>
-          <Row>
-            <Col md={11} >
+    <Container>
+      <Grid fluid>
+        <Row>
+          <Col md={6} >
+            <div className="clearfix" style={{ marginBottom: '25px' }}>
+              <img className="pull-left" src={logo} alt="logo" />
+            </div>
+            <Row>
+              <Col md={12} >
+                <Copy>
+                  Who We Are
+                </Copy>
 
-              <Copy>
-                Who We Are
-              </Copy>
+                <Copy>
+                  The founders of Farm Envy come from both sides of the CSA process.
+                  Farmers with more than 10 years of experience running an active CSA
+                  and customers that know what is expected from the sign-up process.
+                </Copy>
 
-              <Copy>
-                The founders of Farm Envy come from both sides of the CSA process.
-                Farmers with more than 10 years of experience running an active CSA
-                and customers that know what is expected from the sign-up process.
-              </Copy>
+                <Copy>
+                  We understand that growing the produce is often the fun and easy part
+                  but managing a successful CSA can be the hardest and most stressful part
+                  of all. Farm Envy simplifies this process in one user-friendly site.
+                  We have taken all of our frustrations and have created the solution.
+                </Copy>
 
-              <Copy>
-                We understand that growing the produce is often the fun and easy part
-                but managing a successful CSA can be the hardest and most stressful part
-                of all. Farm Envy simplifies this process in one user-friendly site.
-                We have taken all of our frustrations and have created the solution.
-              </Copy>
+                <Copy>
+                  Get ready because your world is about to get a whole lot easier.
+                </Copy>
 
-              <Copy>
-                Get ready because your world is about to get a whole lot easier.
-              </Copy>
+                <Copy>
+                  Coming Soon…
+                </Copy>
+              </Col>
+            </Row>
+          </Col>
 
-              <Copy>
-                Coming Soon…
-              </Copy>
-            </Col>
-          </Row>
-        </Col>
-
-        <Col md={6} >
-          <p>
-            CSA management made easy.
-            Enter your information below to get early access.
-          </p>
-        </Col>
-      </Row>
-    </Grid>
+          <Col md={6} >
+            <SignupText>
+              CSA management made easy.
+              Enter your information below to get early access.
+            </SignupText>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
   </ThemeProvider>
 );
 
