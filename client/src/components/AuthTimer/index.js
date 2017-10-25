345dae2abc133e578717467d26d02bbfba5b2c71
John Mason <john@johnmason.io>
2017-10-25 12:39:52 -0400

    do not wrap with a div if not necessary

--- client/src/components/AuthTimer/index.js
+++ client/src/components/AuthTimer/index.js
@@ -33,11 +33,7 @@ class AuthTimer extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    );
+    return this.props.children;
   }
 }
 
