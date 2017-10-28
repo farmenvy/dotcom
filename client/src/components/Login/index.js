d4c5067497c18aab895980f2c001025cefd36575
John Mason <john@johnmason.io>
2017-10-27 20:44:44 -0400

    shake it baby

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { Redirect } from 'react-router-dom';
 import { Glyphicon } from 'react-bootstrap';
 import { connect } from 'react-redux';
@@ -11,6 +11,7 @@ import {
   login,
 } from '../../interactions/auth';
 import { Box, Layout, Button } from '../SimplePage';
+import Shaker from '../Shaker';
 
 const Header = styled.p`
   font-size: 26px;
@@ -55,34 +56,6 @@ const IconWrapper = styled.span`
   top: 47%;
 `;
 
-const shake = keyframes`
-  10%, 90% {
-    transform: translate3d(-1px, 0, 0);
-  }
-
-  20%, 80% {
-    transform: translate3d(2px, 0, 0);
-  }
-
-  30%, 50%, 70% {
-    transform: translate3d(-4px, 0, 0);
-  }
-
-  40%, 60% {
-    transform: translate3d(4px, 0, 0);
-  }
-`;
-
-const Shaker = styled.div`
-  &:hover {
-    animation: ${shake} 0.82s cubic-bezier(.36,.07,.19,.97) both;
-    transform: translate3d(0, 0, 0);
-    backface-visibility: hidden;
-    perspective: 1000px;
-  }
-`;
-
-
 const Login = (props) => {
   const { from } = props.location.state || { from: { pathname: '/' } };
 
@@ -94,7 +67,7 @@ const Login = (props) => {
 
   return (
     <Layout>
-      <Shaker>
+      <Shaker className={props.isError && 'shake'}>
         <Box>
           <Header>Login</Header>
           <InputWrapper>
@@ -137,17 +110,20 @@ Login.propTypes = ({
     state: PropTypes.shape({}),
   }).isRequired,
   isLoggedIn: PropTypes.bool.isRequired,
+  isError: PropTypes.bool,
 });
 
 Login.defaultProps = ({
   email: '',
   password: '',
+  isError: false,
 });
 
 const mapStateToProps = state => ({
   email: state.auth.email,
   password: state.auth.password,
   isLoggedIn: state.auth.isLoggedIn,
+  isError: state.auth.isError,
 });
 
 const mapDispatchToProps = dispatch => ({
