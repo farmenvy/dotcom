85723e2406419ca6142870a317c0f6af1bf7e1ae
John Mason <john@johnmason.io>
2017-11-01 12:59:23 -0400

    add plus sign to create button

--- client/src/components/CSAManager/pickup.js
+++ client/src/components/CSAManager/pickup.js
@@ -8,10 +8,27 @@ const Foo = styled.button`
   background-color: rgba(241, 245, 241, 0.25);
   border: solid 1px #e5f0e7;
   outline: none;
+
+  &:before {
+    content: '+';
+    margin-right: 8px;
+    display: inline-block;
+    width: 18px;
+    height: 18px;
+    font-size: 12px;
+    font-weight: bold;
+    background-color: rgba(51, 152, 113, 0.15);
+    color: #379f77;
+    border: solid 1px #379f77;
+    border-radius: 100%;
+  }
 `;
 
+
 const Pickup = () => (
-  <Foo>Create New Pickup Location</Foo>
+  <Foo>
+    Create New Pickup Location
+  </Foo>
 );
 
 export default Pickup;
