b874318aeb14f8fcb6c08d4cc1fc966644b8e12d
John Mason <john@johnmason.io>
2017-11-03 19:30:59 -0400

    wip

--- client/src/components/CSAManager/basics.js
+++ client/src/components/CSAManager/basics.js
@@ -1,47 +1,65 @@
 import React from 'react';
-import { Input, Row, Col } from '../common';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import DatePicker from 'material-ui/DatePicker';
+import { MuiThemeProvider } from 'material-ui/styles';
+import TextField from 'material-ui/TextField';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { Row, Col } from '../common';
+
+const muiTheme = getMuiTheme({
+  fontFamily: "'Open Sans', sans-serif",
+  palette: {
+    primary1Color: '#3EB989',
+    primary2Color: '#3EB989',
+    accent1Color: '#3EB989',
+    pickerHeaderColor: '#33658A',
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 const Basics = () => (
-  <div>
-    <Row>
-      <Input
-        type="text"
-        title="Name of this CSA"
-        value="Spring 2018"
-        error="this field is required"
-        readOnly
-      />
-    </Row>
-
-    <Row>
-      <Col>
-        <Input
-          type="text"
-          title="When does this CSA begin?"
-          value="Spring 2018"
-          readOnly
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <div>
+      <Row>
+        <TextField
+          floatingLabelText="CSA name"
         />
-      </Col>
+      </Row>
 
-      <Col>
-        <Input
-          type="text"
-          title="When does this CSA begin?"
-          value="Spring 2018"
-          readOnly
+      <Row>
+        <Col>
+          <DatePicker floatingLabelText="CSA start" />
+        </Col>
+
+        <Col>
+          <DatePicker floatingLabelText="CSA end" minDate={new Date()} />
+        </Col>
+      </Row>
+
+      <Row>
+        <TextField
+          hintText="Password Field"
+          floatingLabelText="Password"
+          type="password"
         />
-      </Col>
-    </Row>
 
-    <Row>
-      <Input
-        type="text"
-        title="Does this CSA offer weekly, biweekly, or both?"
-        value="Spring 2018"
-        readOnly
-      />
-    </Row>
-  </div>
+        <SelectField
+          floatingLabelText="Frequency"
+          value="2"
+          multiple
+        >
+          <MenuItem value={1} primaryText="Never" />
+          <MenuItem value={2} primaryText="Every Night" />
+          <MenuItem value={3} primaryText="Weeknights" />
+          <MenuItem value={4} primaryText="Weekends" />
+          <MenuItem value={5} primaryText="Weekly" />
+        </SelectField>
+      </Row>
+    </div>
+  </MuiThemeProvider>
 );
 
 export default Basics;
