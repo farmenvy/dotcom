0077d8e66c896c8b310f82eb804a8ba42d8a1e02
John Mason <john@johnmason.io>
2017-11-03 20:23:02 -0400

    dont run unless in production

--- api/app/interactions/grow_bot_notify.rb
+++ api/app/interactions/grow_bot_notify.rb
@@ -5,6 +5,7 @@ class GrowBotNotify
 
   def call
     context.fail!(error: 'no payload') unless context.payload
+    context.fail!(error: 'not production') unless Rails.env.production?
     res = HTTParty.post(GROWBOT_URL, body: context.payload.to_json)
     context.response = res.body
 
